03f07e25e6c7b8e39463dc07f557ec2049747b49
Akhilesh Halageri <akhileshhalageri@gmail.com>
2021-07-15 16:07:50 +0000

    fix: set int as str false

--- pcgl2cache/app/v1/routes.py
+++ pcgl2cache/app/v1/routes.py
@@ -56,7 +56,7 @@ def attr_metadata():
 
 @bp.route("/table/<table_id>/attributes", methods=["POST"])
 def attributes(table_id):
-    int64_as_str = request.args.get("int64_as_str", default=True, type=toboolean)
+    int64_as_str = request.args.get("int64_as_str", default=False, type=toboolean)
     return jsonify_with_kwargs(
         common.handle_attributes(table_id), int64_as_str=int64_as_str
     )

684a03b57424e9d8f8c341295d13c11984daf16b
Akhilesh Halageri <akhileshhalageri@gmail.com>
2021-08-09 00:56:05 +0000

    wip: test IDs

--- workers/l2_feats_missing.py
+++ workers/l2_feats_missing.py
@@ -6,6 +6,7 @@ def callback(payload):
     from pcgl2cache.core.calc_l2_feats import run_l2cache
 
     l2ids = np.frombuffer(payload.data, dtype=np.uint64)
+    l2ids = np.array([175137943013294114, 175137943013294118], dtype=np.uint64)
     table_id = payload.attributes["table_id"]
     l2_cache_id = payload.attributes["l2_cache_id"]
     print(table_id, l2_cache_id, l2ids)

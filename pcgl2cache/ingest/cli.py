5296aaa7708cdef02196763285ed0563751f2551
Akhilesh Halageri <akhileshhalageri@gmail.com>
2021-09-20 18:01:17 +0000

    wip: pcgv2 ingest

--- pcgl2cache/ingest/cli.py
+++ pcgl2cache/ingest/cli.py
@@ -25,7 +25,7 @@ def flush_redis():
 @click.argument("timestamp", type=str)
 @click.option("--create", is_flag=True)
 @click.option("--test", is_flag=True)
-def ingest_graph(
+def ingest_cache(
     cache_id: str, graph_id: str, cv_path: str, timestamp: str, create: bool, test: bool
 ):
     """
@@ -36,6 +36,44 @@ def ingest_graph(
     from . import IngestConfig
     from . import ClusterIngestConfig
     from .v1.jobs import enqueue_atomic_tasks
+
+    if create:
+        from kvdbclient import BigTableClient
+
+        client = BigTableClient()
+        client.create_table(cache_id)
+
+    # example format Jun 1 2005 1:33PM
+    timestamp = datetime.strptime(timestamp, "%b %d %Y %I:%M%p")
+    enqueue_atomic_tasks(
+        IngestionManager(
+            IngestConfig(CLUSTER=ClusterIngestConfig(), TEST_RUN=test),
+            cache_id,
+            graph_id,
+        ),
+        cv_path,
+        timestamp,
+    )
+
+
+@ingest_cli.command("v2")
+@click.argument("cache_id", type=str)
+@click.argument("graph_id", type=str)
+@click.argument("cv_path", type=str)
+@click.argument("timestamp", type=str)
+@click.option("--create", is_flag=True)
+@click.option("--test", is_flag=True)
+def ingest_cache_v2(
+    cache_id: str, graph_id: str, cv_path: str, timestamp: str, create: bool, test: bool
+):
+    """
+    Main ingest command
+    Takes ingest config from a yaml file and queues atomic tasks
+    """
+    from datetime import datetime
+    from . import IngestConfig
+    from . import ClusterIngestConfig
+    from .v2.jobs import enqueue_atomic_tasks
 
     if create:
         from kvdbclient import BigTableClient
